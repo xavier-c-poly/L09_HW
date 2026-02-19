48cb203ad7b7f3bd587d2fa41e2b643107d3d34b
Xavier Cosme <xcosme1849@floridapoly.edu>
2026-02-19 00:53:28 +0000

    Finished Crazyball and Print_Iface implementation!

--- main.py
+++ main.py
@@ -48,6 +48,7 @@ class Cannonball:
         self._y = 0
         self._vx = 0
         self._vy = 0
+        self._print_iface = Print_Iface()
 
     ## Move the cannon ball, using its current velocities.
     #  @param sec the amount of time that has elapsed.
@@ -82,27 +83,61 @@ class Cannonball:
         self._vy = velocity * sin(angle)
         self.move(step, user_grav)
 
-        xs = []
-        ys = []
-
         while self.getY() > 1e-14:
-            xs.append(self.getX())
-            ys.append(self.getY())
+            self._print_iface.addX(self.getX())
+            self._print_iface.addY(self.getY())
             self.move(step, user_grav)
+    def getXs(self):
+        return self._print_iface.getXsInterface()
+    def getYs(self):
+        return self._print_iface.getYsInterface()
+    
+    # Print the chart for the trajectory
+    # @param app the streamlit app
+    #
+    def printChart(self, app):
+        df = pd.DataFrame({"x": self.getXs(), "y": self.getYs()})
+        
+        #### debug start
+        # df = pd.DataFrame({"x": [10, 20, 30, 40, 50, 60, 70, 80], "y": [0, 40, 60, 80, 60, 40, 20, 0]})
+        # print("START Lets find out stuff..")
+        # print(self.getXs())
+        # print("\ngetXs() above\n")
+        # print(self.getYs())
+        # print("getYs() above\n")
+        # print(self.getXs()[1])
+        # print("getXs()[1] above\n")
+        # print(self.getYs()[1])
+        # print("getYs()[1] above\n")
+        # print("Lets find out stuff.. END\n")
+        #### debug end
 
-        return xs, ys
+        chart = (
+            alt.Chart(df)
+            .mark_line()
+            .encode(
+                x=alt.X("x:Q", scale=alt.Scale(domain=[0, 200]), title="Distance (m)"),
+                y=alt.Y("y:Q", scale=alt.Scale(domain=[0, 100]), title="Height (m)")
+            )
+            .properties(width=700, height=400)
+        )
+        app.altair_chart(chart, use_container_width=True)
 
 
 
 class Crazyball(Cannonball):
     def __init__(self, x):
         super().__init__(x)
-        print_iface = Print_Iface()
+    
+    ## Move the cannon ball, using its current velocities.
+    #  @param sec the amount of time that has elapsed.
+    #  @param grav the amount of gravity acting on the ball.
+    #
     def move(self, sec, grav):
-        self.rand_g = random.randrange(0, 10)
+        # Generate the random gravity and determine if its applied
+        self.rand_g = random.randrange(0, 15)
         if self.getX() < 400:
-            # "add random values"?
-            pass
+            grav = self.rand_g
 
         dx = self._vx * sec
         dy = self._vy * sec
@@ -115,7 +150,17 @@ class Crazyball(Cannonball):
 
 
 class Print_Iface:
-    pass
+    def __init__(self):
+        self._xs = []
+        self._ys = []
+    def addX(self, new_x):
+        self._xs.append(new_x)
+    def addY(self, new_y):
+        self._ys.append(new_y)
+    def getXsInterface(self):
+        return self._xs
+    def getYsInterface(self):
+        return self._ys
 
 
 
@@ -134,28 +179,23 @@ def run_app():
 
     col1, col2 = st.columns(2)
     simulate = col1.button("Simulate")
+    crazy_simulate = col2.button("Crazy Simulate")
 
-    if simulate:
+    if simulate or crazy_simulate:
         angle_rad = radians(angle_deg)
-        ball = Cannonball(0)
-        xs, ys = ball.shoot(angle_rad, velocity, gravity, step)
 
-        if not xs:
+        if simulate:
+            ball = Cannonball(0)
+        elif crazy_simulate:
+            ball = Crazyball(0)
+
+        ball.shoot(angle_rad, velocity, gravity, step)
+
+        if not ball.getXs():
             st.warning("No trajectory points were generated.")
             return
 
-        df = pd.DataFrame({"x": xs, "y": ys})
-
-        chart = (
-            alt.Chart(df)
-            .mark_line()
-            .encode(
-                x=alt.X("x:Q", scale=alt.Scale(domain=[0, 200]), title="Distance (m)"),
-                y=alt.Y("y:Q", scale=alt.Scale(domain=[0, 100]), title="Height (m)")
-            )
-            .properties(width=700, height=400)
-        )
-        st.altair_chart(chart, use_container_width=True)
+        ball.printChart(st)
 
 
 if __name__ == "__main__":
